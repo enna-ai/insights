25a2aaa9712d92d1e90794bd2964ea5f75ff4644
enna-ai <annelee3322@gmail.com>
2024-01-23 13:36:31 -0700

    ✨ add funcs to compare lists

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/enna-ai/instacheck
 go 1.21.6
 
 require (
-	github.com/labstack/echo/v4 v4.11.4 // indirect
+	github.com/labstack/echo/v4 v4.11.4 // direct
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"text/template"
@@ -9,7 +11,25 @@ import (
 )
 
 type PageVariables struct {
-	Message string
+	Message               string
+	UsersNotFollowingBack []string
+	NotFollowingUsers      []string
+}
+
+type User struct {
+	Href      string `json:"href"`
+	Value     string `json:"value"`
+	Timestamp int    `json:"timestamp"`
+}
+
+type FollowersFile struct {
+	Title          string        `json:"title"`
+	MediaListData  []interface{} `json:"media_list_data"`
+	StringListData []User        `json:"string_list_data"`
+}
+
+type FollowingFile struct {
+	RelationshipsFollowing []FollowersFile `json:"relationships_following"`
 }
 
 func handler(c echo.Context) error {
@@ -26,13 +46,105 @@ func notFound(c echo.Context) error {
 
 func main() {
 	e := echo.New()
-	
+
 	e.Renderer = NewTemplateRenderer("public/views/*.html")
 
 	e.GET("/", handler)
 	e.GET("/*", notFound)
 
+	e.POST("/upload", func(c echo.Context) error {
+		followersFile, err := c.FormFile("followers")
+		if err != nil {
+			fmt.Printf("Error getting followers file: %s\n", err)
+			return c.String(http.StatusBadRequest, "Error getting followers file")
+		}
+
+		followersData, err := followersFile.Open()
+		if err != nil {
+			fmt.Printf("Error opening followers file: %s\n", err)
+			return c.String(http.StatusInternalServerError, "Error opening followers file")
+		}
+		defer followersData.Close()
+
+		followingFile, err := c.FormFile("following")
+		if err != nil {
+			fmt.Printf("Error getting following file: %s\n", err)
+			return c.String(http.StatusBadRequest, "Error getting following file")
+		}
+
+		followingData, err := followingFile.Open()
+		if err != nil {
+			fmt.Printf("Error opening following file: %s\n", err)
+			return c.String(http.StatusInternalServerError, "Error opening following file")
+		}
+		defer followingData.Close()
+
+		var followers []FollowersFile
+		err = json.NewDecoder(followersData).Decode(&followers)
+		if err != nil {
+			fmt.Printf("Error decoding followers data: %s\n", err)
+			return c.String(http.StatusInternalServerError, "Error decoding followers data")
+		}
+
+		var following FollowingFile
+		err = json.NewDecoder(followingData).Decode(&following)
+		if err != nil {
+			fmt.Printf("Error decoding following data: %s\n", err)
+			return c.String(http.StatusInternalServerError, "Error decoding following data")
+		}
+
+		notFollowingBack := findUsersImNotFollowing(following.RelationshipsFollowing, followers)
+		fmt.Printf("Users i'm not following back: %+v\n", notFollowingBack)
+	
+		usersNotFollowingBack := findNotFollowingBack(followers, following.RelationshipsFollowing)
+		fmt.Printf("Users not following back: %+v\n", usersNotFollowingBack)
+
+		pageVariables := PageVariables{
+			Message:          "Files Uploaded",
+			UsersNotFollowingBack: usersNotFollowingBack,
+			NotFollowingUsers: notFollowingBack,
+		}
+
+		return c.Render(http.StatusOK, "main.html", pageVariables)
+	})
+
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func findNotFollowingBack(followers []FollowersFile, following []FollowersFile) []string {
+	followersMap := make(map[string]bool)
+
+	for _, follower := range followers {
+		followersMap[follower.StringListData[0].Value] = true
+	}
+
+	var usersNotFollowingMe []string
+	for _, follow := range following {
+		if _, ok := followersMap[follow.StringListData[0].Value]; !ok {
+			usersNotFollowingMe = append(usersNotFollowingMe, follow.StringListData[0].Value)
+		}
+	}
+
+	return usersNotFollowingMe
+}
+
+func findUsersImNotFollowing(followings []FollowersFile, followers []FollowersFile) []string {
+    followingsMap := make(map[string]bool)
+
+    for _, following := range followings {
+        followingsMap[following.StringListData[0].Value] = true
+    }
+
+    var notFollowingBack []string
+    for _, follower := range followers {
+        if _, ok := followingsMap[follower.StringListData[0].Value]; !ok {
+            notFollowingBack = append(notFollowingBack, follower.StringListData[0].Value)
+        }
+    }
+
+    fmt.Printf("Users I'm not following back: %+v\n", notFollowingBack) // Logging statement
+
+    return notFollowingBack
 }
 
 func NewTemplateRenderer(dir string) *TemplateRenderer {
